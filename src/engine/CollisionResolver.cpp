625857c0d8dabbf1cfce4ec666d606b32dbc2526
Guillem Calabuig Domenech <ge64mel@mytum.de>
2021-05-27 19:49:29 +0200

    some final cleaning

--- src/engine/CollisionResolver.cpp
+++ src/engine/CollisionResolver.cpp
@@ -142,58 +142,6 @@ void gp::engine::CollisionResolver::applyCollisionImpulseWithoutRotationFriction
 	//assert(abs(momentumDiff.x()) <= EPSILON && abs(momentumDiff.y()) <= EPSILON && abs(momentumDiff.z()) <= EPSILON);
 
 
-
-	
-
-
-	//______________OLD_________________
-	//if(obj1->isMovable() && obj2->isMovable()) {
-	//	Vector3f n = m_collision.collisionNormal();
-	//	float_t Ekin1Before = obj1->mass()*v1.dot(v1)/2.0f;
-	//	float_t Ekin2Before = obj2->mass()*v2.dot(v2)/2.0f;
-	//	float_t Ekin1After = obj1->mass()*(v1New+v1).dot(v1New+v1)/2.0f;
-	//	float_t Ekin2After = obj2->mass()*(v2New+v2).dot(v2New+v2)/2.0f;
-//
-	//	Vector3f momentumBefore1 = obj1->mass()*v1;
-	//	Vector3f momentumBefore2 = obj2->mass()*v2;
-	//	Vector3f momentumAfter1 = obj1->mass()*(v1New+v1);
-	//	Vector3f momentumAfter2 = obj2->mass()*(v2New+v2);
-	//	/*float_t beforeM = m_collision.collisionNormal().dot(obj1->mass()*v1 + obj2->mass()*v2);
-	//	float_t afterM = m_collision.collisionNormal().dot(obj1->mass()*obj1->velocity() + obj2->mass()*obj2->velocity());
-	//	float_t diffM = abs(beforeM-afterM);*/
-	//	Vector3f mDiff =  (momentumAfter1+ momentumAfter2) - (momentumBefore1 + momentumBefore2); 
-	//	
-	//	float_t ourEkinLoss = (Ekin1After + Ekin2After) - (Ekin1Before + Ekin2Before); 
-//
-	//	//Checking momentum conservation
-	//	if(mDiff != engine::Vector3f::Ones()*0) {
-	//	//	std::cout << mDiff << std::endl;
-	//	}
-//
-//
-	//	float_t theorEkinLoss = -((1.0f-COF*COF) / 2.0f);
-	//	theorEkinLoss*=((v1-v2).dot(m_collision.collisionNormal()));
-	//	theorEkinLoss*=((v1-v2).dot(m_collision.collisionNormal()));
-	//	theorEkinLoss/=(invM1 + invM2);
-//
-	//	//Checking kinetic energy loss
-	//	if (std::abs(ourEkinLoss - theorEkinLoss) >= EPSILON) {
-	//		std::cout << std::abs(ourEkinLoss - theorEkinLoss) << "  " << EPSILON << std::endl;
-	//	}
-	//	//assert(std::abs(ourDiff - theoryDiff) < EPSILON);
-	//}else if (obj1->isMovable()){
-	//	float_t beforeM = m_collision.collisionNormal().dot(obj1->mass()*v1);
-	//	float_t afterM = m_collision.collisionNormal().dot(obj1->mass()*obj1->velocity());
-	//	float_t diffM = COF*abs(beforeM)-abs(afterM);
-	//	if(diffM>EPSILON)std::cout<<diffM<<std::endl;
-	//}else if (obj2->isMovable()){
-	//	float_t beforeM = m_collision.collisionNormal().dot(obj2->mass()*v2);
-	//	float_t afterM = m_collision.collisionNormal().dot(obj2->mass()*obj2->velocity());
-	//	float_t diffM = COF*abs(beforeM)-abs(afterM);
-	//	if(diffM>EPSILON)std::cout<<diffM<<std::endl;
-	//}else{
-	//	//std::cout<<"both immovable"<<std::endl;
-	//	}
 }
 
 void gp::engine::CollisionResolver::applyCollisionImpulseWithoutFriction()
