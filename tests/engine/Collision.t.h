4860d6a3b0348a6d6b835c7f830d37b5a2c6f67b
Alexander <ge25wes@mytum.de>
2021-06-10 19:27:32 +0200

    preliminary test helpers added to boxbox

--- tests/engine/Collision.t.h
+++ tests/engine/Collision.t.h
@@ -43,16 +43,54 @@ public:
 		gp::engine::Object* o1 = &box1;
 		gp::engine::Object* o2 = &box2;
 		gp::engine::Collision coll(o1, o2);
-		bool ret = coll.detect();
-		//TS_ASSERT_EQUALS(ret,true);
-		// collPoint is on the sphere
-		//std::cout<<"This is a Test "  << coll.collisionPoint1();
-		//TS_ASSERT_DELTA((coll.collisionPoint1()-sphere.position()).norm(),sphere.radius(),gp::engine::EPSILON);
-		// interpentration depth is distance between collision points
-		//TS_ASSERT_DELTA((coll.collisionPoint2()-coll.collisionPoint1()).norm(),coll.interpenetrationDepth(),gp::engine::EPSILON);
-		//collNormal is normalized
-		//TS_ASSERT_DELTA(coll.collisionNormal().norm(),1.,gp::engine::EPSILON);
-		//TS_ASSERT(false);
+		
+		
 	}
-	
+	private:
+	static void helperVPBoxBox(const gp::engine::Box &box1,const gp::engine::Box &box2, gp::engine::Collision coll)
+	{
+		bool ret = coll.detect();
+		//collisionNormal is normalized
+		TS_ASSERT_DELTA(coll.collisionNormal().norm(),1.,gp::engine::EPSILON);
+
+		gp::engine::Vector3f normal = coll.collisionPoint2()-coll.collisionPoint1();
+		float_t interpen = normal.norm();
+		//interpentration depth is distance between collision points
+		TS_ASSERT_DELTA(coll.interpenetrationDepth(),interpen,gp::engine::EPSILON);
+
+		//normal points from point 1 to point 2
+		normal.normalize();
+		TS_ASSERT_DELTA(coll.collisionNormal().x(),normal.x(),gp::engine::EPSILON);
+		TS_ASSERT_DELTA(coll.collisionNormal().y(),normal.y(),gp::engine::EPSILON);
+		TS_ASSERT_DELTA(coll.collisionNormal().z(),normal.z(),gp::engine::EPSILON);
+
+		//one collision point is inside one box
+
+		//one collision point is on a box plane
+		gp::engine::Vector3f plane_b1p1 = box1.invModelMatrix()*coll.collisionPoint1();
+
+		
+		
+	}
+	static void helperEEBoxBox(const gp::engine::Box &box1,const gp::engine::Box &box2, gp::engine::Collision coll)
+	{
+		bool ret = coll.detect();
+		//collisionNormal is normalized
+		TS_ASSERT_DELTA(coll.collisionNormal().norm(),1.,gp::engine::EPSILON);
+
+		gp::engine::Vector3f normal = coll.collisionPoint2()-coll.collisionPoint1();
+		float_t interpen = normal.norm();
+		//interpentration depth is distance between collision points
+		TS_ASSERT_DELTA(coll.interpenetrationDepth(),interpen,gp::engine::EPSILON);
+
+		//normal points from point 1 to point 2
+		normal.normalize();
+		TS_ASSERT_DELTA(coll.collisionNormal().x(),normal.x(),gp::engine::EPSILON);
+		TS_ASSERT_DELTA(coll.collisionNormal().y(),normal.y(),gp::engine::EPSILON);
+		TS_ASSERT_DELTA(coll.collisionNormal().z(),normal.z(),gp::engine::EPSILON);
+
+		//both collision points are on an edge of a box
+		
+
+	}
 };
