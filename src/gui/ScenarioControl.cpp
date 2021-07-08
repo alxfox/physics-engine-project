10c4296f31c19c4c571729f9ed15de331f8fe198
Korbi <ge36mok@mytum.de>
2021-07-08 03:25:05 +0200

    changing the ui, the 3 possible levels are the 3 cutom models

--- src/gui/ScenarioControl.cpp
+++ src/gui/ScenarioControl.cpp
@@ -12,36 +12,36 @@ gp::gui::ScenarioControl::ScenarioControl(GLFWwindow* window, gp::messages::Queu
 	initialize(window, true);
 
 	FormHelper *gui = new FormHelper(this);
-	ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Scenario");
+	ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Menu");
 
-  gui->addGroup("Select");
-  gui->addButton("Scenario 1", [this]() { loadScenario<gp::Scenario1>(); });
-  gui->addButton("Box box test", [this]() { loadScenario<gp::BoxBoxTest>(); });
-  gui->addButton("Many spheres", [this]() { loadScenario<gp::ManySpheres>(); });
-  gui->addButton("Many cubes", [this]() { loadScenario<gp::ManyCubes>(); });
-  gui->addButton("Hanging bridge", [this]() { loadScenario<gp::HangingBridge>(); });
-  gui->addButton("Pendulum", [this]() { loadScenario<gp::Pendulum>(); });
-  gui->addButton("Custom 1", [this]() { loadScenario<gp::Custom1>(); });
-  gui->addButton("Custom 2", [this]() { loadScenario<gp::Custom2>(); });
-  gui->addButton("Custom 3", [this]() { loadScenario<gp::Custom3>(); });
+  gui->addGroup("Choose difficulty");
+//   gui->addButton("Scenario 1", [this]() { loadScenario<gp::Scenario1>(); });
+//   gui->addButton("Box box test", [this]() { loadScenario<gp::BoxBoxTest>(); });
+//   gui->addButton("Many spheres", [this]() { loadScenario<gp::ManySpheres>(); });
+//   gui->addButton("Many cubes", [this]() { loadScenario<gp::ManyCubes>(); });
+//   gui->addButton("Hanging bridge", [this]() { loadScenario<gp::HangingBridge>(); });
+//   gui->addButton("Pendulum", [this]() { loadScenario<gp::Pendulum>(); });
+  gui->addButton("Easy peasy", [this]() { loadScenario<gp::Custom1>(); });
+  gui->addButton("Normal", [this]() { loadScenario<gp::Custom2>(); });
+  gui->addButton("Insane", [this]() { loadScenario<gp::Custom3>(); });
 
-	gui->addGroup("Simulation");
+	gui->addGroup("Pause Game (P)");
 	Widget* simulation = new Widget(nanoguiWindow.get());
 	simulation->setLayout(new BoxLayout(Orientation::Horizontal, Alignment::Middle));
 	m_playPause = new Button(simulation, "");
 	m_playPause->setIcon(ENTYPO_ICON_PAUS);
 	m_playPause->setCallback(std::bind(&ScenarioControl::toggleEngine, this));
-	m_nextFrame = new Button(simulation, "");
-	m_nextFrame->setIcon(ENTYPO_ICON_TO_END);
-	m_nextFrame->setEnabled(false);
-	m_nextFrame->setCallback(std::bind(&ScenarioControl::stepEngine, this));
+	// m_nextFrame = new Button(simulation, "");
+	// m_nextFrame->setIcon(ENTYPO_ICON_TO_END);
+	// m_nextFrame->setEnabled(false);
+	// m_nextFrame->setCallback(std::bind(&ScenarioControl::stepEngine, this));
 	gui->addWidget("", simulation);
 
-	gui->addGroup("Render options");
-	gui->addVariable("Ropes", enableConstraintsRendering);
+	// gui->addGroup("Render options");
+	// gui->addVariable("Ropes", enableConstraintsRendering);
 
 	nanogui::Label *instructions = new nanogui::Label(nanoguiWindow.get(),
-		 "Left-click game canvas and move mouse for rotating the camera. Press W, A, S, D, E, Q to move around. Press P to pause and press COMMA to advance to the next time step when paused. Press H to toggle gui visibility.");
+		 "Press W, A, S, D to move and Left Mouseclick to shoot. \n \nTry to shoot the flying objects as far away from you as possible so that they disappear. \n \n Press H to hide the Menue.");
 	instructions->setFixedWidth(150);
 	gui->addGroup("Instructions");
 	gui->addWidget("", instructions);
@@ -49,6 +49,8 @@ gp::gui::ScenarioControl::ScenarioControl(GLFWwindow* window, gp::messages::Queu
 	setVisible(true);
 	performLayout();
 }
+
+
 
 gp::gui::ScenarioControl::~ScenarioControl()
 {
@@ -64,7 +66,7 @@ void gp::gui::ScenarioControl::toggleEngine()
 	} else {
 		m_playPause->setIcon(ENTYPO_ICON_PLAY);
 	}
-	m_nextFrame->setEnabled(m_paused);
+	// m_nextFrame->setEnabled(m_paused);
 	m_vis2engine.push(gp::engine::messages::ControlMessage(m_paused));
 }
 
